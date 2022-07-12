8316a2bdef9bf83887d699e65800c4bd6f9e8103
Aramis Tanelus <aramis.tanelus@gmail.com>
2022-07-12 16:43:22 -0400

    Fix typos

--- run_batch.sh
+++ run_batch.sh
@@ -5,8 +5,8 @@
 #SBATCH -c 1
 #SBATCH --gpus=1
 #SBATCH --mem=32768mb
-#SBATCH --time=1:00:00
-#SBATCH --array=1-40%4
+#SBATCH --time=1:30:00
+#SBATCH --array=1-500%4
 #SBATCH -o slurm_logs/train_model_%a.log
 pwd; hostname; date;
 
@@ -21,13 +21,13 @@ pwd; hostname; date;
 
 # Expects the data dir as first positional argument
 # config path/name as second argument
-DATA_DIR=$1
-BATCH_DIR=$2
+# DATA_DIR=$1
+BATCH_DIR=$1
 
-if [ -z $DATA_DIR ]; then
-    echo "Path to train/val/test datasets should be provided as the first positional argument"
-    exit 1
-fi
+# if [ -z $DATA_DIR ]; then
+#     echo "Path to train/val/test datasets should be provided as the first positional argument"
+#     exit 1
+# fi
 
 if [ -z $BATCH_DIR ]; then
     echo "Path to directory containing config files should be provided as the second positional argument"
@@ -39,7 +39,7 @@ fi
 #    exit 1
 #fi
 
-FMT_BATCH_IDX=$(python3 /mnt/home/atanelus/scripts/pad_integer 4 ${SLURM_ARRAY_TASK_ID})
+FMT_BATCH_IDX=$(python3 /mnt/home/atanelus/scripts/pad_integer.py 4 ${SLURM_ARRAY_TASK_ID})
 
 # Note, config file should include path to data file under DATAFILE_PATH key
 # Else this will crash
