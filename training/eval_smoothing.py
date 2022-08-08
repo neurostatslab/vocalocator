c32a0887e21f76437203f12d665ca5d0cdfa3ebc
Aman Choudhri <amanchoudhri@gmail.com>
2022-08-08 16:07:00 -0400

    Fully phase out prespecified calibration params

--- training/eval_smoothing.py
+++ training/eval_smoothing.py
@@ -22,27 +22,6 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 
-# # ================ calibration constants ==================
-# # gaussian mixture smoothing:
-# # min and max variance used in the gaussian smoothing
-# # of point estimates, in cm
-# MIN_SIGMA = 0.1
-# MAX_SIGMA = 20
-# # number of sigma values at which to calculate
-# # calibration, up to MAX_SIGMA.
-# NUM_STEPS = 50
-# # dynamic spherical gaussian smoothing:
-# # minimium and maximum proportion of the observed
-# # mean distance between each point estimate and the
-# # centroid, used as variance of a gaussian placed
-# # at the centroid
-# MIN_FRAC = 0.01
-# MAX_FRAC = 10
-# # number of bins used to calculate each calibration curve
-# NUM_CALIBRATION_BINS = 20
-# # ========================================================
-
-
 def get_args():
     parser = argparse.ArgumentParser()
 
@@ -89,82 +68,6 @@ def get_args():
         )
     )
 
-    # parser.add_argument(
-    #     '--min_std',
-    #     type=float,
-    #     required=False,
-    #     default=MIN_SIGMA,
-    #     help=(
-    #         'Min smoothing std, in cm, to use in calibration curve calculation. '
-    #         'Calibration curves will be calculated at `num_std_steps` values from '
-    #         f'[min_std, max_std]. Default: {MIN_SIGMA}.'
-    #     )
-    # )
-
-    # parser.add_argument(
-    #     '--max_std',
-    #     type=float,
-    #     required=False,
-    #     default=MAX_SIGMA,
-    #     help=(
-    #         'Max smoothing std, in cm, to use in calibration curve calculation. '
-    #         'Calibration curves will be calculated at `num_std_steps` values from '
-    #         f'[min_std, max_std]. Default: {MAX_SIGMA}.'
-    #     )
-    # )
-
-    # parser.add_argument(
-    #     '--num_std_steps',
-    #     type=int,
-    #     required=False,
-    #     default=NUM_STEPS,
-    #     help=(
-    #         'Number of steps in range at which to calculate the calibration curve '
-    #         'and error for the `gaussian_mixture` smoothing method. '
-    #         f'Default: {NUM_STEPS}'
-    #         )
-    # )
-
-    # parser.add_argument(
-    #     '--min_frac',
-    #     type=float,
-    #     required=False,
-    #     default=MIN_FRAC,
-    #     help=(
-    #         'Minimum fraction to use in calibration curve calculation for the '
-    #         '`dynamic_spherical_gaussian` smoothing method. This smoothing '
-    #         'method calculates the mean distance between each point estimate '
-    #         'and their centroid, then uses various fractions of that value '
-    #         'as the variance of a spherical Gaussian placed at the centroid.'
-    #     )
-    # )
-
-    # parser.add_argument(
-    #     '--max_frac',
-    #     type=float,
-    #     required=False,
-    #     default=MAX_FRAC,
-    #     help=(
-    #         'Minimum fraction to use in calibration curve calculation for the '
-    #         '`dynamic_spherical_gaussian` smoothing method. This smoothing '
-    #         'method calculates the mean distance between each point estimate '
-    #         'and their centroid, then uses various fractions of that value '
-    #         'as the variance of a spherical Gaussian placed at the centroid.'
-    #     )
-    # )
-
-    # parser.add_argument(
-    #     '--num_frac_steps',
-    #     type=int,
-    #     required=False,
-    #     default=NUM_STEPS,
-    #     help=(
-    #         'Number of steps in range at which to calculate the calibration '
-    #         'curve and error for the `dynamic_spherical_gaussian` smoothing '
-    #         f'method. Default: {NUM_STEPS}'
-    #     )
-    # )
-
     args = parser.parse_args()
     validate_args(args)
     return args
@@ -198,6 +101,27 @@ def validate_args(args):
         args.config_data = build_config_from_file(args.config, args.job_id)
 
 
+def load_calibration_config(config_path):
+    """
+    Load in the calibration config dictionary from the provided
+    path to a JSON file.
+    """
+    with open(config_path, 'r') as f:
+        calibration_config = json.load(f)
+
+    def convert_list_to_np(subdict) -> dict:
+        """Helper to convert list subentries to np.arrays."""
+        for k, v in subdict.items():
+            if type(v) == list:
+                subdict[k] = np.array(v)
+            elif type(v) == dict:
+                convert_list_to_np(v)
+    
+    convert_list_to_np(calibration_config)
+
+    return calibration_config     
+
+
 def run():
     args = get_args()
     device = 'gpu' if torch.cuda.is_available() else 'cpu'
@@ -222,7 +146,12 @@ def run():
             args.config_data['ARENA_LENGTH']
             )
         make_xcorr = args.config_data['COMPUTE_XCORRS']
-        test_set = GerbilVocalizationDataset(args.datafile, segment_len=args.config_data['SAMPLE_LEN'], arena_dims=arena_dims, make_xcorrs=make_xcorr)
+        test_set = GerbilVocalizationDataset(
+            args.datafile,
+            segment_len=args.config_data['SAMPLE_LEN'],
+            arena_dims=arena_dims,
+            make_xcorrs=make_xcorr
+            )
         test_set.samp_size = 30  # take 30 samples from each vocalization. Pass them to the model as if each were a full batch of inputs
         #test_set_loader = DataLoader(test_set, args.config_data['TEST_BATCH_SIZE'], shuffle=False)
         test_set_loader = DataLoader(test_set, 1, shuffle=False)  # Only using Dataloader here for the convenience of converting np.ndarray to torch.Tensor
@@ -249,35 +178,15 @@ def run():
         # convert arena dims to cm (same unit as the output)
         MM_TO_CM = 0.1
         arena_dims_cm = np.array(arena_dims) * MM_TO_CM
-        # set up calibration accumulator
-        # PMF_GRID_RESOLUTION = 1  # 1 cm grid resolution for pmfs
-        # sigma_values = np.linspace(
-        #     args.min_std, args.max_std, args.num_std_steps
-        # )
-        # frac_values = np.linspace(
-        #     args.min_frac, args.max_frac, args.num_frac_steps
-        # )
-
-        # smoothing_specs = {
-        #     'model_output': {
-        #         'gaussian_mixture': {
-        #             'std_values': sigma_values,
-        #             'desired_resolution': PMF_GRID_RESOLUTION,
-        #         },
-        #         'dynamic_spherical_gaussian': {
-        #             'fracs_of_est_variance': frac_values,
-        #             'desired_resolution': PMF_GRID_RESOLUTION,
-        #         }
-        #     }
-        # }
 
         if args.calibration_config:
-            with open(args.calibration_config, 'r') as f:
-                calibration_config = json.load(f)
+            c_config = load_calibration_config(args.calibration_config)
+            logging.debug(f'calibration config: {c_config}')
             ca = CalibrationAccumulator(
                 arena_dims_cm,
-                calibration_config 
+                c_config 
             )
+            OUTPUT_NAME = list(c_config.keys())[0]
 
         with torch.no_grad():
             for idx, (audio, location) in enumerate(test_set_loader):
@@ -317,8 +226,9 @@ def run():
                     if idx % 500 == 0:
                         save_path = Path(args.outdir) / 'pmfs' / f'vox_{idx}'
                         save_path.mkdir(parents=True, exist_ok=True)
+
                     ca.calculate_step(
-                        {'model_output': centered_output},
+                        {OUTPUT_NAME: centered_output},
                         centered_location,
                         pmf_save_path=save_path,
                     )
@@ -334,7 +244,7 @@ def run():
             # calculate the calibration curves + errors
             ca.calculate_curves_and_error(h5_file=dest)
             # plot the figures
-            fig_path = dest_path.parent
+            fig_path = Path(dest_path).parent
             ca.plot_results(fig_path)
 
 
