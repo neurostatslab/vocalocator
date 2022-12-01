398be4c48fc552a170fc557753464e15e920c1d9
Aman Choudhri <amanchoudhri@gmail.com>
2022-12-01 16:11:11 -0500

    Allow user to modify model config for finetuning
    Add ability to pass in a json file to end_to_end.sh that will update
    the model configuration before finetuning. This may be useful for
    something like adjusting the learning rate, for example.

--- end_to_end.sh
+++ end_to_end.sh
@@ -11,7 +11,7 @@ pwd; hostname; date;
 
 ##################################################
 # Usage:
-# sbatch end_to_end.sh pretrain_data_directory finetune_data_directory [model_config]
+# sbatch end_to_end.sh pretrain_data_dir finetune_data_dir [output_dir] [model_config] [finetune_changes]
 # Example:
 # sbatch end_to_end.sh \
 #     /mnt/ceph/users/achoudhri/simulated_vox/small_room_correct_dims \
@@ -21,14 +21,17 @@ pwd; hostname; date;
 # config.json should contain the CONFIG_NAME key
 ##################################################
 
-# Expects the pretrain data dir as first argument
-# Expects the finetune data dir as second argument
-# Output base directory (model output will be stored under a job_id subdirectory)
-# config path as an optional third argument
+# Arg 1: pretrain data dir
+# Arg 2: finetune data dir
+# Arg 3 (optional): output base dir (model output will be stored under a job_id subdirectory)
+# Arg 4 (optional): path to config for model being trained
+# Arg 5 (optional): path to json file storing desired changes to config for finetuning
+
 PRETRAIN_DIR=$1
 FINETUNE_DIR=$2
 OUTPUT_BASE_DIR=$3
 CONFIG=$4
+FINETUNE_CHANGES=$5
 
 if [ -z $PRETRAIN_DIR ]; then
     echo "Path to directory containing train/val/test datasets should be provided as the first positional argument"
@@ -104,9 +107,17 @@ FINETUNE_RESULT_DIR=$OUTPUT_DIR/finetune
 
 echo "Finetuning model on data in directory ${FINETUNE_DIR}."
 
+# update config for finetuning if desired
+CONFIG_FOR_FINETUNING=$PRETRAINED_CONFIG
+
+if [ ! -z $FINETUNE_CHANGES ]; then
+    CONFIG_FOR_FINETUNING=$PRETRAIN_DIR/config_for_finetuning.json
+    python -m gerbilizer.update_json $PRETRAINED_CONFIG $FINETUNE_CHANGES $CONFIG_FOR_FINETUNING
+fi
+
 # finetune the model
 python -u -m gerbilizer.main \
-    --config $PRETRAINED_CONFIG \
+    --config $CONFIG_FOR_FINETUNING \
     --data $FINETUNE_DIR \
     --save_path $FINETUNE_RESULT_DIR \
     --bare
