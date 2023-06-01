991a7083fcff35e9079f29ae3743592f7a4866c8
Aman Choudhri <amanchoudhri@gmail.com>
2023-06-01 11:52:08 -0400

    Remove unused configuration option in ensemble class

--- gerbilizer/architectures/ensemble.py
+++ gerbilizer/architectures/ensemble.py
@@ -32,7 +32,6 @@ class GerbilizerEnsemble(nn.Module):
             model.load_state_dict(weights, strict=False)
 
         self.models = nn.ModuleList(built_models)
-        self.average_outputs = bool(config.get("AVERAGE_OUTPUTS"))
 
 #     @classmethod
 #     def from_ensemble_dir(cls, ensemble_dir):
@@ -67,38 +66,5 @@ class GerbilizerEnsemble(nn.Module):
             means[:, i] = output[:, 0]
             cholesky_covs[:, i] = output[:, 1:]
 
-        # based on the model configuration we either just return these
-        # means and covs
-        if not self.average_outputs:
-            # add extra dim so means is of shape (batch_size, len(self.models), 1, 2)
-            return torch.cat((means[:, :, None], cholesky_covs), dim=-2)
-
-        # or whether we make a "simplifying" assumption
-        # as in (Lakshminarayanan, 2017) and say the mixture density is just
-        # a Gaussian
-
-        # calculate the mean and covariance matrix of the mixture density
-        # mean is just the average of the means:
-        mixture_means = means.mean(dim=1)
-
-        # by introducing a new variable z representing the mixture
-        # assignment, we can use the law of total variance to decompose the
-        # covariance matrix as Var(Y) = E[Var(Y | Z = z)] + Var(E[Y | Z = z])
-
-        # Cholesky factor of E[Var(Y|Z=z)]
-        avg_cholesky_cov = cholesky_covs.mean(dim=1)
-        # Var(E[Y | Z = z])
-        cov_means_over_assigment = torch.zeros(batch_size, 2, 2)
-        for i in range(batch_size):
-            # take the transpose since torch.cov expects rows to be variables
-            # and columns to be observations
-            # here, one observation is one predicted mean from a given model
-            cov_means_over_assigment[i] = torch.cov(means[i].T)
-        # take the cholesky decomposition
-        chol_cov_over_assignment = torch.linalg.cholesky(cov_means_over_assigment)
-        # sum the two to find the full cholesky mixture covariance
-        chol_mixture_cov = avg_cholesky_cov + chol_cov_over_assignment
-        # concatenate mixture_means and chol_mixture_cov to make a (batch, 3, 2) tensor
-        # reshape mixture_means to be (batch_size, 1, 2)
-        return torch.cat((mixture_means[:, None], chol_mixture_cov), dim=-2)
-
+        # add extra dim so means is of shape (batch_size, len(self.models), 1, 2)
+        return torch.cat((means[:, :, None], cholesky_covs), dim=-2)
