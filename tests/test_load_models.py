fae04c738aa5bb0572a914bfb3a49768ffe7f4f2
Aman Choudhri <amanchoudhri@gmail.com>
2023-06-01 11:52:47 -0400

    Update tests for model + ensemble initialization

--- tests/test_load_models.py
+++ tests/test_load_models.py
@@ -5,15 +5,12 @@ Test the build model functions.
 import unittest
 
 import torch
-
-from gerbilizer.architectures.simplenet import GerbilizerSimpleNetwork
-from gerbilizer.architectures.ensemble import GerbilizerEnsemble
+from gerbilizer.training.models import build_model
 
 from constants import (
     SIMPLENET_BASE,
     SIMPLENET_COV,
     ENSEMBLE,
-    ENSEMBLE_AVG,
     ENSEMBLE_MISSING_COV,
 )
 
@@ -24,13 +21,13 @@ class TestOuptutShapes(unittest.TestCase):
         # number of channels of input with four mics and passed-in cross correlations
         NUM_CHANNELS_MICS_AND_XCORR = 10
         cls.fake_input = torch.zeros(
-            1, NUM_CHANNELS_MICS_AND_XCORR, SIMPLENET_BASE["SAMPLE_LEN"]
+            1, SIMPLENET_BASE["SAMPLE_LEN"], NUM_CHANNELS_MICS_AND_XCORR
         )
 
     def test_simplenet(self):
         # test loading the models
-        cnn_no_cov = GerbilizerSimpleNetwork(SIMPLENET_BASE)
-        cnn_cov = GerbilizerSimpleNetwork(SIMPLENET_COV)
+        cnn_no_cov, _ = build_model(SIMPLENET_BASE)
+        cnn_cov, _ = build_model(SIMPLENET_COV)
 
         no_cov_expected_shape = (1, 2)
         cov_expected_shape = (1, 3, 2)
@@ -39,15 +36,11 @@ class TestOuptutShapes(unittest.TestCase):
         self.assertEqual(cnn_cov(self.fake_input).shape, cov_expected_shape)
 
     def test_ensemble(self):
-        ensemble = GerbilizerEnsemble(ENSEMBLE)
+        ensemble, _ = build_model(ENSEMBLE)
         expected_shape = (1, len(ENSEMBLE["MODELS"]), 3, 2)
         self.assertEqual(ensemble(self.fake_input).shape, expected_shape)
-
-        avgd_expected_shape = (1, 3, 2)
-        avg_ensemble = GerbilizerEnsemble(ENSEMBLE_AVG)
-        self.assertEqual(avg_ensemble(self.fake_input).shape, avgd_expected_shape)
 
         # test that the class throws an error if one of the models
         # doesn't have the 'OUTPUT_COV' flag
         with self.assertRaises(ValueError):
-            GerbilizerEnsemble(ENSEMBLE_MISSING_COV)
+            build_model(ENSEMBLE_MISSING_COV)
