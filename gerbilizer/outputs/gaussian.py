3bcbc1e6a5bdaa3b6f869be0bf6ddd74821a5e71
Aman Choudhri <amanchoudhri@gmail.com>
2023-06-12 12:19:08 -0400

    Separate Gaussian output into multiple subclasses
    This change was motivated by the desire to combine multiple response
    distributions together as in a Mixture Density Network. Doing so seems
    to require that each response distribution object knows how many raw
    numbers it will expect from the model to parameterize the distribution,
    on a class variable rather than instance-specific level.
    
    Before, the umbrella GaussianOutput class expected different numbers
    of parameters depending on the choice of parameterization, which didn't
    fit this requirement.
    
    Also in the interest of cleanliness, the base classes (ModelOutput,
    ProbabilisticOutput, etc) were separated from these new gaussian output
    classes and these files were put into a subdirectory.

---
+++ gerbilizer/outputs/gaussian.py
@@ -0,0 +1,187 @@
+import enum
+import logging
+
+from typing import List, Literal, Optional
+
+import torch
+
+from torch.nn import functional as F
+
+from gerbilizer.outputs.base import ProbabilisticOutput, Unit
+
+
+class GaussianOutput(ProbabilisticOutput):
+    """
+    Base class representing a (batch of) Gaussian response distribution(s) that
+    packages model output together with its units.
+    """
+
+    N_OUTPUTS_EXPECTED: int
+
+    def __init__(
+        self,
+        raw_output: torch.Tensor,
+        arena_dims: torch.Tensor,
+        arena_dim_units: Unit,
+        ):
+        super().__init__(raw_output, arena_dims, arena_dim_units)
+
+        # expect subclasses to provide this instance variable
+        # in their init methods
+        self.cholesky_covs: torch.Tensor
+
+        if raw_output.shape[-1] != self.N_OUTPUTS_EXPECTED:
+            raise ValueError(
+                f'Given Gaussian output class {self.__class__.__name__}, expected '
+                f'output to have dimension {self.N_OUTPUTS_EXPECTED} '
+                f'along final axis, but instead encountered dimension '
+                f'{raw_output.shape[-1]}.'
+                )
+
+    def point_estimate(self, units: Unit = Unit.ARBITRARY):
+        """
+        Return the mean of the Gaussian(s) in the specified units.
+        """
+        # first two values of model output are always interpreted as the mean
+        mean = torch.clamp(self.data[:, :2], -1, 1)
+        return self._convert(mean, Unit.ARBITRARY, units)
+
+    def _log_p(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Return log p(x) under the Gaussian parameterized by the model
+        output, where x is an array-like with expected shape (..., 2). Importantly,
+        x is expected to be provided in arbitrary units (i.e. on the square [-1, 1]^2).
+        """
+        distr = torch.distributions.MultivariateNormal(
+            loc=self.point_estimate(),
+            scale_tril=self.cholesky_covs
+            )
+        return distr.log_prob(x)
+
+
+class GaussianParameterization(enum.Enum):
+    """
+    Enum class storing different ways to parameterize a Gaussian response and
+    the number of raw model outputs expected in each case.
+    """
+    SPHERICAL_FIXED_VARIANCE = 2
+    SPHERICAL = 3
+    DIAGONAL = 4
+    CHOLESKY = 5
+
+
+def select_gaussian_type(parameterization):
+    try:
+        parameterization = GaussianParameterization[parameterization]
+    except KeyError:
+        raise ValueError(
+            f'Invalid paramterization string {parameterization}! '
+            f'Expected one of {[param.name for param in GaussianParameterization]}.'
+            )
+    return parameterization
+
+                             
+class GaussianOutputFixedVariance(ProbabilisticOutput):
+
+    N_OUTPUTS_EXPECTED = 2
+
+    def __init__(
+        self,
+        raw_output: torch.Tensor,
+        arena_dims: torch.Tensor,
+        arena_dim_units: Unit,
+        variance: torch.Tensor,
+        units: Unit,
+        ):
+        """
+        Construct a GaussianOutput object with fixed spherical covariance
+        matrix across all inputs, with diagonal entries given by `variance` in
+        units `units`.
+        """
+        super().__init__(raw_output, arena_dims, arena_dim_units)
+
+        if not variance or not units:
+            raise ValueError(
+                'For spherical fixed variance parameterization, '
+                'arguments `variance` and `units` are required!'
+                )
+        # if necessary, convert to arbitrary units
+        if units == Unit.ARBITRARY:
+            variances = torch.ones(2) * variance
+        else:
+            variances = (torch.ones(2) * variance) / self.arena_dims[units]
+
+        cholesky_cov = torch.diag_embed(torch.sqrt(variances))
+        self.cholesky_covs = cholesky_cov[None].repeat(self.batch_size, 1, 1)
+
+class GaussianOutputSphericalCov(ProbabilisticOutput):
+
+    N_OUTPUTS_EXPECTED = 3
+
+    def __init__(
+        self,
+        raw_output: torch.Tensor,
+        arena_dims: torch.Tensor,
+        arena_dim_units: Unit,
+        ):
+        """
+        Construct a GaussianOutput object with spherical covariance matrices.
+
+        Unlike GaussianOutputFixedVariance, this class does NOT impose the
+        restriction that covariance matrices are fixed, instead they may vary
+        by input.
+        """
+        super().__init__(raw_output, arena_dims, arena_dim_units)
+
+        # for now, just do a softplus so the diagonal entries of the
+        # Cholesky (lower triangular) factor of the covariance matrix
+        # are positive
+        diagonal_entries = F.softplus(self.data[:, 2] * torch.ones(2))
+        self.cholesky_covs = torch.diag_embed(diagonal_entries)
+
+class GaussianOutputDiagonalCov(ProbabilisticOutput):
+
+    N_OUTPUTS_EXPECTED = 4
+
+    def __init__(
+        self,
+        raw_output: torch.Tensor,
+        arena_dims: torch.Tensor,
+        arena_dim_units: Unit,
+        ):
+        """
+        Construct a GaussianOutput object with diagonal covariance matrices.
+        """
+        super().__init__(raw_output, arena_dims, arena_dim_units)
+
+        # for now, just do a softplus so the diagonal entries of the
+        # Cholesky (lower triangular) factor of the covariance matrix
+        # are positive
+        diagonal_entries = F.softplus(self.data[:, 2:])
+        self.cholesky_covs = torch.diag_embed(diagonal_entries)
+
+class GaussianOutputFullCov(ProbabilisticOutput):
+
+    N_OUTPUTS_EXPECTED = 5
+
+    def __init__(
+        self,
+        raw_output: torch.Tensor,
+        arena_dims: torch.Tensor,
+        arena_dim_units: Unit,
+        ):
+        """
+        Construct a GaussianOutput object with diagonal covariance matrices.
+        """
+        super().__init__(raw_output, arena_dims, arena_dim_units)
+
+        L = torch.zeros(self.batch_size, 2, 2, device=self.device)
+        # embed the elements into the matrix
+        idxs = torch.tril_indices(2, 2)
+        L[:, idxs[0], idxs[1]] = self.data[:, 2:]
+        # apply softplus to the diagonal entries to guarantee the resulting
+        # matrix is positive definite
+        new_diagonals = F.softplus(L.diagonal(dim1=-2, dim2=-1))
+        L = L.diagonal_scatter(new_diagonals, dim1=-2, dim2=-1)
+        # reshape y_hat so we can concatenate it to L
+        self.cholesky_covs = L
