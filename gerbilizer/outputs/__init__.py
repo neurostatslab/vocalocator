053664236c60849720f7f90753e5663c45354532
Aman Choudhri <amanchoudhri@gmail.com>
2023-06-14 15:15:56 -0400

    Bring main output classes to top-level output namespace

---
+++ gerbilizer/outputs/__init__.py
@@ -0,0 +1,12 @@
+from gerbilizer.outputs.factory import ModelOutputFactory
+from gerbilizer.outputs.base import (
+    PointOutput,
+    MDNOutput,
+    UniformOutput
+    )
+from gerbilizer.outputs.gaussian import (
+    GaussianOutputFixedVariance,
+    GaussianOutputSphericalCov,
+    GaussianOutputDiagonalCov,
+    GaussianOutputFullCov
+    )
