07771b1d5de4486c9cb99540e701af4a90cfa3c1
Aman Choudhri <amanchoudhri@gmail.com>
2023-06-16 13:55:15 -0400

    Import architectures to top level architectures namespace
    This is 1) for convenience and 2) to make sure calls to the
    __subclasses__ method of GerbilizerArchitecture (in models.py, for
    instance) pick up all architectures.

---
+++ gerbilizer/architectures/__init__.py
@@ -0,0 +1,5 @@
+from .attentionnet import GerbilizerAttentionNet, GerbilizerSparseAttentionNet
+from .base import GerbilizerArchitecture
+from .ensemble import GerbilizerEnsemble
+from .simplenet import GerbilizerSimpleNetwork
+
